93d66a4c0613883d005a07a1ef65613eccd17fd9
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-17 17:12:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON characters.author_id = authors.id
   GROUP BY characters.species = 'human'
-  ORDER BY LIMIT 1
+  ORDER BY characters.species LIMIT 1
   "
 end
 
